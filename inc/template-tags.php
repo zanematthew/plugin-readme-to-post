8dcddb4b2182cde624af9cac695db549cea7ebf9
Zane <zane@zanematthew.com>
2015-08-03 22:40:58 -0400

    Adding check

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -68,99 +68,102 @@ function plugin_readme_to_post( $atts ) {
         $readme_string = $tmp_readme['body'];
     }
 
-    if ( empty( $readme_string ) ) return;
-
-    $markdown = Markdown( $readme_string );
-    $options = get_option('prtp_settings');
-
-    $image_captions = explode( PHP_EOL, trim( prtp_get_string_between( $readme_string, '== Screenshots ==', '== Changelog ==' ) ) );
-
-    $images_with_captions = array();
-    $tmp = array();
-    $i = 1;
-
-    foreach( $image_captions as $caption ){
-        $tmp = array(
-            'id' => $slug . '_screenshot-' . $i . '.png',
-            'url' => plugin_dir_url( ( dirname( dirname( __FILE__ ) ) ) ) . $slug . '/_assets/screenshot-' . $i . '.png',
-            'width' => '',
-            'height' => '',
-            'caption' => $caption,
-            'alt' => esc_attr( $caption )
-            );
-        $images_with_captions[] = $tmp;
-        $i++;
-    }
-
-    // Build HTML
-    $images_html = null;
-    foreach( $images_with_captions as $image ) {
-        $images_html .= '<figure id="' . $image['id'] . '" style="width: ' . $image['width'] . ';" class="wp-caption alignleft">';
-        $images_html .= '<figcaption class="wp-caption-text">' . $image['caption'] . '</figcaption>';
-        $images_html .= '<a href="' . $image['url'] . '">';
-        $images_html .= '<img class="size-full ' . $image['id'] . '" src="' . $image['url'] . '" alt="' . $image['alt'] . '" width="' . $image['width'] . '" height="' . $image['height'] . '"></a>';
-
-        $images_html .= '</figure>';
-    }
-
-
-    if ( ! empty( $options['tabs'] ) && $options['tabs'] == 1 ){
-        $tmp = explode( '<h2>Changelog</h2>', $markdown );
-        $changelog = $tmp[1];
-
-        $readme_sections = array(
-            array(
-                'name' => 'Images',
-                'content' => $images_html
-                ),
-            array(
-                'name' => 'Description',
-                'content' => prtp_get_string_between( $markdown, '<h2>Description</h2>', '<h2>Installation</h2>' )
-            ),
-            array(
-                'name' => 'Installation',
-                'content' => prtp_get_string_between( $markdown, '<h2>Installation</h2>', '<h2>Frequently Asked Questions</h2>' )
-            ),
-            array(
-                'name' => 'FAQ',
-                'content' => prtp_get_string_between( $markdown, '<h2>Frequently Asked Questions</h2>', '<h2>Screenshots</h2>' )
-                ),
-            array(
-                'name' => 'Changelog',
-                'content' => $changelog
-                )
-            );
-
-        $image_url = plugin_dir_url( dirname( dirname( __FILE__ ) ) ) . sanitize_title( get_the_title( $post->ID ) );
-        $screenshots = glob( plugin_dir_path( dirname( dirname( __FILE__ ) ) ) . sanitize_title( get_the_title( $post->ID ) ) . "/screenshot-*.png", GLOB_BRACE );
-
-        if ( ! empty( $screenshots ) ){
-            $readme_sections[] = array(
-                'name' => 'Screenshots',
-                'content' => prtp_get_string_between( $markdown, '<h2>Screenshots</h2>', '<h2>Changelog</h2>' )
-            );
-        }
-
-        $tabs = null;
-        $content = null;
-        $i = 0;
-
-        foreach( $readme_sections as $section ){
-            $clean_name = str_replace( ' ', '-', strtolower( $section['name'] ) );
-            $tabs .= '<li><a href="#' . $clean_name . '" data-toggle="tab">' . $section['name'] . '</a></li>';
-            $content .= '<div id="' . $clean_name . '" class="tab-pane">' . $section['content'];
-            if ( $section['name'] == 'Screenshots' ){
-                while ( $i <= count( $screenshots ) - 1 ) {
-                    $i++;
-                    $content .= '<img src="' . $image_url . '/screenshot-' . $i . '.png" />';
-                }
-            }
-            $content .= '</div>';
-        }
-
-        $html = '<div class="prtp-tabs"><ul>' . $tabs . '</ul><div>'.$content.'</div></div>';
+    if ( empty( $readme_string ) ) {
+        $html = "Unable to find readme for {$slug}.";
     } else {
-        $html = $markdown;
+
+        $markdown = Markdown( $readme_string );
+        $options = get_option('prtp_settings');
+
+        $image_captions = explode( PHP_EOL, trim( prtp_get_string_between( $readme_string, '== Screenshots ==', '== Changelog ==' ) ) );
+
+        $images_with_captions = array();
+        $tmp = array();
+        $i = 1;
+
+        foreach( $image_captions as $caption ){
+            $tmp = array(
+                'id' => $slug . '_screenshot-' . $i . '.png',
+                'url' => plugin_dir_url( ( dirname( dirname( __FILE__ ) ) ) ) . $slug . '/_assets/screenshot-' . $i . '.png',
+                'width' => '',
+                'height' => '',
+                'caption' => $caption,
+                'alt' => esc_attr( $caption )
+                );
+            $images_with_captions[] = $tmp;
+            $i++;
+        }
+
+        // Build HTML
+        $images_html = null;
+        foreach( $images_with_captions as $image ) {
+            $images_html .= '<figure id="' . $image['id'] . '" style="width: ' . $image['width'] . ';" class="wp-caption alignleft">';
+            $images_html .= '<figcaption class="wp-caption-text">' . $image['caption'] . '</figcaption>';
+            $images_html .= '<a href="' . $image['url'] . '">';
+            $images_html .= '<img class="size-full ' . $image['id'] . '" src="' . $image['url'] . '" alt="' . $image['alt'] . '" width="' . $image['width'] . '" height="' . $image['height'] . '"></a>';
+
+            $images_html .= '</figure>';
+        }
+
+
+        if ( ! empty( $options['tabs'] ) && $options['tabs'] == 1 ){
+            $tmp = explode( '<h2>Changelog</h2>', $markdown );
+            $changelog = $tmp[1];
+
+            $readme_sections = array(
+                array(
+                    'name' => 'Images',
+                    'content' => $images_html
+                    ),
+                array(
+                    'name' => 'Description',
+                    'content' => prtp_get_string_between( $markdown, '<h2>Description</h2>', '<h2>Installation</h2>' )
+                ),
+                array(
+                    'name' => 'Installation',
+                    'content' => prtp_get_string_between( $markdown, '<h2>Installation</h2>', '<h2>Frequently Asked Questions</h2>' )
+                ),
+                array(
+                    'name' => 'FAQ',
+                    'content' => prtp_get_string_between( $markdown, '<h2>Frequently Asked Questions</h2>', '<h2>Screenshots</h2>' )
+                    ),
+                array(
+                    'name' => 'Changelog',
+                    'content' => $changelog
+                    )
+                );
+
+            $image_url = plugin_dir_url( dirname( dirname( __FILE__ ) ) ) . sanitize_title( get_the_title( $post->ID ) );
+            $screenshots = glob( plugin_dir_path( dirname( dirname( __FILE__ ) ) ) . sanitize_title( get_the_title( $post->ID ) ) . "/screenshot-*.png", GLOB_BRACE );
+
+            if ( ! empty( $screenshots ) ){
+                $readme_sections[] = array(
+                    'name' => 'Screenshots',
+                    'content' => prtp_get_string_between( $markdown, '<h2>Screenshots</h2>', '<h2>Changelog</h2>' )
+                );
+            }
+
+            $tabs = null;
+            $content = null;
+            $i = 0;
+
+            foreach( $readme_sections as $section ){
+                $clean_name = str_replace( ' ', '-', strtolower( $section['name'] ) );
+                $tabs .= '<li><a href="#' . $clean_name . '" data-toggle="tab">' . $section['name'] . '</a></li>';
+                $content .= '<div id="' . $clean_name . '" class="tab-pane">' . $section['content'];
+                if ( $section['name'] == 'Screenshots' ){
+                    while ( $i <= count( $screenshots ) - 1 ) {
+                        $i++;
+                        $content .= '<img src="' . $image_url . '/screenshot-' . $i . '.png" />';
+                    }
+                }
+                $content .= '</div>';
+            }
+
+            $html = '<div class="prtp-tabs"><ul>' . $tabs . '</ul><div>'.$content.'</div></div>';
+        } else {
+            $html = $markdown;
+        }
     }
 
     return '<div class="prtp-container">'.$html.'</div>';
